2225349c11ee97b2d9f6ca24986414cee9b18af7
夏轩 <xiaxuan@weibangong.com>
2016-12-03 10:09:38 +0800

    基本完成dubbox相关开发

--- dubbox-goods-web/src/main/java/cn/com/dubbo/goods/controllers/OrderController.java
+++ dubbox-goods-web/src/main/java/cn/com/dubbo/goods/controllers/OrderController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by xiaxuan on 16/12/1.
  */
@@ -21,6 +24,10 @@ public class OrderController {
                     consumes = "application/json;charset=utf-8",
                     produces = "application/json;charset=utf-8")
     public Object addOrder(@RequestBody Order order) {
-        return null;
+        orderService.addOrder(order);
+        Map<String, Object> result = new HashMap<>();
+        result.put("status", 200);
+        result.put("msg", "hello world");
+        return result;
     }
 }
